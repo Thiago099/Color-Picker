c8955e64b068476d63eda5d47444edb2d261e2f6
Thiago Kaique Ribeiro Fonseca <66787043+Thiago099@users.noreply.github.com>
2020-12-27 14:44:55 -0300

    Add files via upload

---
+++ source/Color Picker/Panel2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+namespace Color_Picker
+{
+    class Panel2:Panel
+    {
+        Color Border_color = SystemColors.Control;
+
+        public Color border_color
+        {
+            get => Border_color;
+            set 
+            {
+                Border_color = value;
+                Refresh();
+            }
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, ClientRectangle, Border_color, ButtonBorderStyle.Solid);
+        }
+    }
+    
+}
